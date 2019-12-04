203a6b58c02ddaea53ce2dfbcf812993a5828bcd
luisdev97 <amadormontorodavid@gmail.com>
2019-12-04 23:05:05 +0100

    Get data from Weather Map API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Error from './components/Error';
 
 function App() {
 
+  const API_KEY = "c399ffd80d49b64d640c05b8171958ab";
   const [ city, setCity ] = useState('');
   const [ country, setCountry ] = useState('');
+  const [ result, setResult ] = useState({});
   const [ validationError, setValidationError] = useState(false);
 
+  //Set App state from Formulario
   const dataSearch = data => {
 
     const { city, country } = data;
@@ -23,10 +26,24 @@ function App() {
       setValidationError(false);
   }
 
+  //Get Data from Weather Map API
+  const getWeatherAPI = async() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`; 
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    setResult(weatherData);
+  }
+
+  //ComponentDIdMount and ComponenDidUpdate
+  useEffect(() => {
+    city && getWeatherAPI();
+  },[ city, country ]);
+
   let component = validationError ? <Error messagge="All fields are required"/> : null;
 
   return (
     <div className="App">
+
       <Header title="React WeatherApp"/>
 
       <div className="contenedor-form">
