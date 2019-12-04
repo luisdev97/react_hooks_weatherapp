b6c2f053b163c2eac339c99dedbd64cf09070dc0
luisdev97 <amadormontorodavid@gmail.com>
2019-12-04 22:44:46 +0100

    Show validation error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import Error from './components/Error';
 
 function App() {
 
   const [ city, setCity ] = useState('');
   const [ country, setCountry ] = useState('');
+  const [ validationError, setValidationError] = useState(false);
 
   const dataSearch = data => {
-    
+
     const { city, country } = data;
 
-    if(city === '' || country === '')
+    if(city === '' || country === ''){
+      setValidationError(true);
       return;
+    }
     
       setCity(city);
       setCountry(country);
+      setValidationError(false);
   }
 
+  let component = validationError ? <Error messagge="All fields are required"/> : null;
 
   return (
     <div className="App">
@@ -26,9 +32,15 @@ function App() {
       <div className="contenedor-form">
           <div className="container">
             <div className="row">
+
               <div className="col s12 m6">
                 <Formulario dataSearch= { dataSearch }/>
               </div>
+
+              <div className="col s12 m6">
+                  { component }
+              </div>
+
             </div>
           </div>
       </div>
